1f81d030c167c33986ef20aebf10e1d7facb5405
mozmmil-ali <mly88207@gmail.com>
2025-04-02 14:22:38 +0200

    Add user authentication views, forms, and dashboard template

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import authenticate, get_user_model
+User = get_user_model()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise forms.ValidationError("Invalid username or password")
--- users/views.py
+++ users/views.py
@@ -1,7 +1,34 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import LoginForm
+
+
+def dashboard_view(request: HttpRequest):
+
+    return render(request, "users/dashboard.html")
 
 
 def login_view(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect("dashboard")
 
-    return render(request, "login.html")
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect("dashboard")
+    else:
+        form = LoginForm()
+
+    return render(request, "users/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
