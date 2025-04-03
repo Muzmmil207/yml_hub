11df6bb34c8ca7bf36554afb5eae21d7d80c5161
mozmmil-ali <mly88207@gmail.com>
2025-04-03 19:02:40 +0200

    Add dashboard and courses views with URL routing; create base template

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import landing_page
+from .views import courses_view, dashboard_view, landing_page
 
 urlpatterns = [
     path("", landing_page, name="landing"),
+    path("dashboard/", dashboard_view, name="dashboard"),
+    path("courses/", courses_view, name="courses"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
-from django.shortcuts import  render
-
+from django.shortcuts import render
 
 def landing_page(request: HttpRequest):
     return render(request, "base/landing-page.html")
+
+
+@login_required
+def dashboard_view(request: HttpRequest):
+    return render(request, "base/dashboard.html")
+
+
+@login_required
+def courses_view(request: HttpRequest):
+    return render(request, "base/courses.html")
